dbe7b53d3e7dc8895b6d5560fd5406e0c9af1a25
duckquack <42347839+duckquack@users.noreply.github.com>
2018-08-21 08:35:10 -0700

    update

--- prefs.php
+++ prefs.php
@@ -1,1 +1,1 @@
-a:10:{s:8:"max_size";i:1800;s:10:"max_enable";i:1;s:8:"add_tags";i:1;s:8:"subtitle";s:55:"Uploaded with [url=https://is.gd/XVG49v]DropToPTP[/url]";s:7:"allowed";a:4:{i:0;s:4:"jpeg";i:1;s:3:"jpg";i:2;s:3:"png";i:3;s:3:"gif";}s:5:"limit";i:10;s:9:"stay_open";i:0;s:15:"no_files_action";i:0;s:12:"after_upload";i:0;s:3:"key";s:0:"";}
+a:4:{s:7:"allowed";a:1:{i:0;s:0:"";}s:8:"add_tags";b:0;s:15:"no_files_action";b:0;s:12:"after_upload";b:0;}
